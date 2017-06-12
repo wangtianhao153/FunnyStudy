cc556b31dff69104ac5db1c355a3cedf1ab4850d
wushilin12 <1972602940@qq.com>
2017-06-12 14:24:28 +0800

    主页

---
+++ funnystudy/app/Http/Controllers/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+
+class IndexController extends Controller
+{
+    Public function index(){
+        $news=DB::table('news')->select('title','picture','time')->limit(6)->get();
+        $course=DB::table('course')->limit(12)->get();
+        $users=DB::table('users')->where('role',1)->limit(4)->get();
+        return view('Index/index')
+                ->with('news',$news)
+                ->with('course',$course)
+                ->with('users',$users);
+    }
+
+
+}
